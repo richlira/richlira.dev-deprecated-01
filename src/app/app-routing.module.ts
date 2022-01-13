67c7c18b4d2a57e5a8208bb4357072e845ffe117
Ricardo Lira <ricardodelavega@gmail.com>
2022-01-13 12:51:26 -0600

    Rich: Removes Hola Banana.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BioComponent } from './bio/bio.component';
-import { HolabananaComponent } from './holabanana/holabanana.component';
-import { HolabananaprivacypolicyComponent } from './holabananaprivacypolicy/holabananaprivacypolicy.component';
-
 
 const routes: Routes = [
   {
     path: "",
     component: BioComponent,
-  },
-  {
-    path: "holabanana",
-    component: HolabananaComponent,
-  },
-  {
-    path: "bananaprivacypolicy",
-    component: HolabananaprivacypolicyComponent,
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HolabananaComponent } from './holabanana/holabanana.component';
 import { BioComponent } from './bio/bio.component';
-import { HolabananaprivacypolicyComponent } from './holabananaprivacypolicy/holabananaprivacypolicy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HolabananaComponent,
     BioComponent,
-    HolabananaprivacypolicyComponent
   ],
   imports: [
     BrowserModule,
